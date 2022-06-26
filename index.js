f1afdf8f4a429ff5e89a685a7cac6d138e67f63f
subhransu <subhransusekharsahu29@gmail.com>
2022-06-27 01:05:18 +0530

    only Promise

--- index.js
+++
@@ -1,5 +0,0 @@
-// 1.javascript data type is of 2 types :- primitive and no
-// 1.string 2.number 3.boolean 4.bigint 5.object 6.undefined 7.null 8.symbol
-// st,sy,nu,bo,bi,ob,un,ob
-// 2.primitive 
-
